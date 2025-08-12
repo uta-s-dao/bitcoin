703a5a75df2b5524be3c08cd38c5ebc602a6596d
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-08-12 17:44:46 +0900

    Add:macd

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -148,6 +148,26 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 		df.AddRsi(period)
 	}
 
+	macd := r.URL.Query().Get("macd")
+	if macd != "" {
+		strPeriod1 := r.URL.Query().Get("macdFastPeriod")
+		strPeriod2 := r.URL.Query().Get("macdSlowPeriod")
+		strPeriod3 := r.URL.Query().Get("macdSignalPeriod")
+		period1, err := strconv.Atoi(strPeriod1)
+		if strPeriod1 == "" || err != nil || period1 < 0 {
+			period1 = 12
+		}
+		period2, err := strconv.Atoi(strPeriod2)
+		if strPeriod2 == "" || err != nil || period2 < 0 {
+			period2 = 26
+		}
+		period3, err := strconv.Atoi(strPeriod3)
+		if strPeriod3 == "" || err != nil || period3 < 0 {
+			period3 = 9
+		}
+		df.AddMacd(period1, period2, period3)
+	}
+
 	js, err := json.Marshal(df)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -1,9 +1,10 @@
 package models
 
 import (
-	"github.com/markcheno/go-talib"
 	"gotrading/tradingalgo"
 	"time"
+
+	"github.com/markcheno/go-talib"
 )
 
 type DataFrameCandle struct {
@@ -15,6 +16,7 @@ type DataFrameCandle struct {
 	BBands        *BBands        `json:"bbands,omitempty"`
 	IchimokuCloud *IchimokuCloud `json:"ichimoku,omitempty"`
 	Rsi           *Rsi           `json:"rsi,omitempty"`
+	Macd          *Macd          `json:"macd,omitempty"`
 }
 
 type Sma struct {
@@ -46,6 +48,15 @@ type IchimokuCloud struct {
 type Rsi struct {
 	Period int       `json:"period,omitempty"`
 	Values []float64 `json:"values,omitempty"`
+}
+
+type Macd struct {
+	FastPeriod   int       `json:"fast_period,omitempty"`
+	SlowPeriod   int       `json:"slow_period,omitempty"`
+	SignalPeriod int       `json:"signal_period,omitempty"`
+	Macd         []float64 `json:"macd,omitempty"`
+	MacdSignal   []float64 `json:"macd_signal,omitempty"`
+	MacdHist     []float64 `json:"macd_hist,omitempty"`
 }
 
 func (df *DataFrameCandle) Times() []time.Time {
@@ -159,4 +170,21 @@ func (df *DataFrameCandle) AddRsi(period int) bool {
 		return true
 	}
 	return false
+}
+
+func (df *DataFrameCandle) AddMacd(inFastPeriod, inSlowPeriod, inSignalPeriod int) bool {
+	if len(df.Candles) > 1 {
+		outMACD, outMACDSignal, outMACDHist := talib.Macd(df.Closes(), inFastPeriod, inSlowPeriod, inSignalPeriod)
+		df.Macd = &Macd{
+			FastPeriod:   inFastPeriod,
+			SlowPeriod:   inSlowPeriod,
+			SignalPeriod: inSignalPeriod,
+			Macd:         outMACD,
+			MacdSignal:   outMACDSignal,
+			MacdHist:     outMACDHist,
+		}
+		return true
+	}
+	return false
+
 }
