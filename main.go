d36bcd59d68d79030ade5c26c0dae8f2b74d844d
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-08-13 18:44:27 +0900

    event model

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
-	"gotrading/app/controllers"
 	"gotrading/app/models"
-	"gotrading/config"
-	"gotrading/utils"
+	"time"
 )
 
+//import (
+//	"gotrading/app/controllers"
+//	"gotrading/config"
+//	"gotrading/utils"
+//)
+//
+//func main(){
+//	utils.LoggingSettings(config.Config.LogFile)
+//	controllers.StreamIngestionData()
+//	controllers.StartWebServer()
+//}
+
 func main() {
-	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
-	controllers.StreamIngestionData() // ← データ取り込み処理
-	controllers.StartWebServer()      // ← Webサーバー起動
+	s := models.NewSignalEvents()
+	df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
+	c1 := df.Candles[0]
+	c2 := df.Candles[5]
+	s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
+	s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
+	fmt.Println(models.GetSignalEventsByCount(1))
+	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
+	fmt.Println(s.CollectAfter(time.Now().UTC()))
+	fmt.Println(s.CollectAfter(c1.Time))
 }
