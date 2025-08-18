2913645202d68d4936fe2ee40eb7999da8d4ba4a
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-08-18 14:49:22 +0900

    133

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -138,6 +138,7 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 		// Volume data is already included in Candle struct, no additional processing needed
 	}
 
+	//売られすぎ買われすぎ
 	rsi := r.URL.Query().Get("rsi")
 	if rsi != "" {
 		strPeriod := r.URL.Query().Get("rsiPeriod")
@@ -193,12 +194,11 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 	events := r.URL.Query().Get("events")
 	if events != "" {
 		if config.Config.BackTest {
-			performance, p1, p2, p3 := df.OptimizeMacd()
+			performance, p1, p2, p3 := df.OptimizeRsi()
 			log.Println(performance, p1, p2, p3)
 			if performance > 0 {
-				df.Events = df.BackTestMacd(p1, p2, p3)
+				df.Events = df.BackTestRsi(p1, p2, p3)
 			}
-
 		} else {
 			firstTime := df.Candles[0].Time
 			df.AddEvents(firstTime)
