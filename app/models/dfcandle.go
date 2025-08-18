e2948aac1b8e7d03643e9d96eba0023619c1cc15
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2025-08-18 15:20:45 +0900

    134

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -1,10 +1,11 @@
 package models
 
 import (
+	"github.com/markcheno/go-talib"
+	"sort"
 	"time"
 
-	"github.com/markcheno/go-talib"
-
+	"gotrading/config"
 	"gotrading/tradingalgo"
 )
 
@@ -430,9 +431,80 @@ func (df *DataFrameCandle) OptimizeRsi() (performance float64, bestPeriod int, b
 		if performance < profit {
 			performance = profit
 			bestPeriod = period
-			bestBuyThread = bestBuyThread
-			bestSellThread = bestSellThread
 		}
 	}
 	return performance, bestPeriod, bestBuyThread, bestSellThread
+}
+
+type TradeParams struct {
+	EmaEnable        bool
+	EmaPeriod1       int
+	EmaPeriod2       int
+	BbEnable         bool
+	BbN              int
+	BbK              float64
+	IchimokuEnable   bool
+	MacdEnable       bool
+	MacdFastPeriod   int
+	MacdSlowPeriod   int
+	MacdSignalPeriod int
+	RsiEnable        bool
+	RsiPeriod        int
+	RsiBuyThread     float64
+	RsiSellThread    float64
+}
+
+type Ranking struct {
+	Enable      bool
+	Performance float64
+}
+
+func (df *DataFrameCandle) OptimizeParams() *TradeParams {
+	emaPerformance, emaPeriod1, emaPeriod2 := df.OptimizeEma()
+	bbPerformance, bbN, bbK := df.OptimizeBb()
+	macdPerformance, macdFastPeriod, macdSlowPeriod, macdSignalPeriod := df.OptimizeMacd()
+	ichimokuPerforamcne := df.OptimizeIchimoku()
+	rsiPerformance, rsiPeriod, rsiBuyThread, rsiSellThread := df.OptimizeRsi()
+
+	emaRanking := &Ranking{false, emaPerformance}
+	bbRanking := &Ranking{false, bbPerformance}
+	macdRanking := &Ranking{false, macdPerformance}
+	ichimokuRanking := &Ranking{false, ichimokuPerforamcne}
+	rsiRanking := &Ranking{false, rsiPerformance}
+
+	rankings := []*Ranking{emaRanking, bbRanking, macdRanking, ichimokuRanking, rsiRanking}
+	sort.Slice(rankings, func(i, j int) bool { return rankings[i].Performance > rankings[j].Performance })
+
+	isEnable := false
+	for i, ranking := range rankings {
+		if i >= config.Config.NumRanking {
+			break
+		}
+		if ranking.Performance > 0 {
+			ranking.Enable = true
+			isEnable = true
+		}
+	}
+	if !isEnable {
+		return nil
+	}
+
+	tradeParams := &TradeParams{
+		EmaEnable:        emaRanking.Enable,
+		EmaPeriod1:       emaPeriod1,
+		EmaPeriod2:       emaPeriod2,
+		BbEnable:         bbRanking.Enable,
+		BbN:              bbN,
+		BbK:              bbK,
+		IchimokuEnable:   ichimokuRanking.Enable,
+		MacdEnable:       macdRanking.Enable,
+		MacdFastPeriod:   macdFastPeriod,
+		MacdSlowPeriod:   macdSlowPeriod,
+		MacdSignalPeriod: macdSignalPeriod,
+		RsiEnable:        rsiRanking.Enable,
+		RsiPeriod:        rsiPeriod,
+		RsiBuyThread:     rsiBuyThread,
+		RsiSellThread:    rsiSellThread,
+	}
+	return tradeParams
 }
